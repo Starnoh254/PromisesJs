38a2a1f825146c5dc4902b099ad669fc9f75589f
Starnoh254 <stajohn697@gmail.com>
2024-01-30 23:45:16 +0300

    added fibonacci series

--- dsa.js
+++ dsa.js
@@ -5,4 +5,21 @@ const isPalindrome = (int) => {
 }
 // console.log(isPalindrome(1210))
 
-module.exports = { isPalindrome } 
+// Fibonacci series 
+const  fibonacciSeries = n =>{
+    if(n<=0) throw new Error('Input should be greater than zero')
+
+    else{
+        let arr=[0,1]
+        for(let i=2;i<n;i++){
+            arr.push(arr[i-1] + arr[i-2])
+        }
+
+        return arr
+    }
+    
+}
+
+console.log(fibonacciSeries(5))
+
+module.exports = { isPalindrome , fibonacciSeries } 
--- dsa.test.js
+++ dsa.test.js
@@ -1,6 +1,7 @@
 
-const { isPalindrome } = require('./dsa');
+const { isPalindrome , fibonacciSeries } = require('./dsa');
 const { describe } = require('node:test');
+
 
 describe('My test ' , () => {
     it('check whether an integer is a palindrome' , () => {
@@ -8,5 +9,8 @@ describe('My test ' , () => {
     });
     it('should return false if the number is not a palindrome',()=>{
         expect(isPalindrome(123)).toBe(false)
+    });
+    it('should return [0 , 1 , 1 , 2 , 3] for input 5' , () => {
+        expect(fibonacciSeries(5)).toStrictEqual([0 , 1 , 1 , 2 , 3])
     })
 })
